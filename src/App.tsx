883a378c784ef0d33c400caf19cb66ed63ccb3d4
Koki Takahashi <hakatasiloving@gmail.com>
2024-09-10 13:37:58 +0000

    Initialize firebase

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,29 @@ import type { Component } from 'solid-js';
 import logo from './logo.svg';
 import styles from './App.module.css';
 
+import {Quiz} from './lib/firebase';
+import { addDoc } from 'firebase/firestore';
+
 const App: Component = () => {
+  const handleClick = async () => {
+    const quiz = await addDoc(Quiz, {
+      title: 'My Quiz',
+      questions: [
+        {
+          question: 'What is 2 + 2?',
+          answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false },
+            { text: '5', correct: false },
+            { text: '10', correct: false },
+          ],
+        },
+      ],
+    });
+
+    console.log('Quiz added with ID: ', quiz.id);
+  };
+
   return (
     <div class={styles.App}>
       <header class={styles.header}>
@@ -11,14 +33,7 @@ const App: Component = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <button onClick={handleClick}>Add Quiz</button>
       </header>
     </div>
   );
---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app';
+import {getFirestore, connectFirestoreEmulator, collection} from 'firebase/firestore';
+
+const firebaseConfigResponse = await fetch('/__/firebase/init.json');
+const firebaseConfig = await firebaseConfigResponse.json();
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+if (location.hostname === 'localhost') {
+  connectFirestoreEmulator(db, 'localhost', 8080);
+}
+
+const Quiz = collection(db, 'quizzes');
+
+export {app as default, db, Quiz};
